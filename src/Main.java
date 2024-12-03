d6aa9798061a92cf39e5e7f0064642326701e283
Vasya-ne-bey <onekillpunch@mail.com>
2024-12-03 03:01:39 +0300

    Сделал 9 домашку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int[] payments = {350, 200, 250, 300, 400};
+        int allPayments = 0;
+        for (int i : payments) {
+            allPayments += i;
+        }
+        System.out.println(allPayments + " 000 рублей за месяц");
+        System.out.println("Задача 2");
+        int maxPayment = -1;
+        for (final int i : payments) {
+            if (i > maxPayment) {
+                maxPayment = i;
+            }
+        }
+        System.out.println(maxPayment + " 000 рублей составит максимальная выплата");
+        for (final int i : payments) {
+            if (i < maxPayment) {
+                maxPayment = i;
+            }
+        }
+        System.out.println(maxPayment + " 000 рублей составит минимальная выплата");
+        System.out.println("Задача 3");
+        double middle = allPayments / payments.length;
+        System.out.println(middle + " тысяч рублей составит средняя выплата за неделю");
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'i', ' ', 'v', 'o', 'n', 'a', 'v', 'i'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
